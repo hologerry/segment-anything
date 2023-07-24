0a0687b4bc80bb2bec32fda19a5335eab8adac00
Yue Gao <hologerry@gmail.com>
2023-07-24 15:51:26 +0800

    update

--- amg_bc_all.py
+++ amg_bc_all.py
@@ -91,7 +91,11 @@ def main(args: argparse.Namespace) -> None:
                 cv2.imwrite(out_mask_path, max_iou_mask)
                 # print(f"Saved mask to '{out_mask_path}' with iou = {max_iou}")
             else:
-                print(f"part [{args.part_idx}/{args.part_num}] Skipping '{bc_color_path}' with iou = {max_iou}")
+                if os.path.exists(out_mask_path):
+                    print(f"part [{args.part_idx}/{args.part_num}] Removing '{out_mask_path}' with iou = {max_iou}")
+                    os.remove(out_mask_path)
+                else:
+                    print(f"part [{args.part_idx}/{args.part_num}] Skipping '{bc_color_path}' with iou = {max_iou}")
 
     print("Done!")
 
