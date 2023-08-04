cd28de8f58438e870060e0ed1fd9f7a329e8238e
Yue Gao <hologerry@gmail.com>
2023-08-04 18:21:39 +0800

    update

--- amg_dreds_bc_all.py
+++ amg_dreds_bc_all.py
@@ -110,8 +110,8 @@ if __name__ == "__main__":
     parser.add_argument("--rand", action="store_true")
 
     # ['default', 'vit_h', 'vit_l', 'vit_b']
-    parser.add_argument("--model_type", type=str, default="default")
-    parser.add_argument("--checkpoint", type=str, default="pretrained_models/sam_vit_h_4b8939.pth")
+    parser.add_argument("--model_type", type=str, default="vit_b")
+    parser.add_argument("--checkpoint", type=str, default="pretrained_models/sam_vit_b_01ec64.pth")
     parser.add_argument("--device", type=str, default="cuda", help="The device to run generation on.")
 
     parser.add_argument("--convert-to-rle", action="store_true")
